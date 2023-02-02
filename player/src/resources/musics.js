b3945e04ff1e948308a021045da4a8907b562e1a
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2023-02-01 22:21:41 -0300

    feat: add new musics

--- player/src/resources/musics.js
+++ player/src/resources/musics.js
@@ -88,5 +88,311 @@ export const music = [
         artist: "Artista 15",
         duration: "4:41",
         src: "http://spoti4.future4.com.br/15.mp3" 
+    },
+    {
+        title: "Música 16",
+        artist: "Artista 16",
+        duration: "2:55",
+        src: "http://spoti4.future4.com.br/16.mp3" 
+    },
+    {
+        title: "Música 17",
+        artist: "Artista 17",
+        duration: "2:31",
+        src: "http://spoti4.future4.com.br/17.mp3" 
+    },
+    {
+        title: "Música 18",
+        artist: "Artista 18",
+        duration: "2:58",
+        src: "http://spoti4.future4.com.br/18.mp3" 
+    },
+    {
+        title: "Música 19",
+        artist: "Artista 19",
+        duration: "2:24",
+        src: "http://spoti4.future4.com.br/19.mp3" 
+    },
+    {
+        title: "Música 20",
+        artist: "Artista 20",
+        duration: "3:24",
+        src: "http://spoti4.future4.com.br/20.mp3" 
+    },
+    {
+        title: "Música 21",
+        artist: "Artista 21",
+        duration: "2:02",
+        src: "http://spoti4.future4.com.br/21.mp3" 
+    },
+    {
+        title: "Música 22",
+        artist: "Artista 22",
+        duration: "2:12",
+        src: "http://spoti4.future4.com.br/22.mp3" 
+    },
+    {
+        title: "Música 23",
+        artist: "Artista 23",
+        duration: "2:40",
+        src: "http://spoti4.future4.com.br/23.mp3" 
+    },
+    {
+        title: "Música 24",
+        artist: "Artista 24",
+        duration: "3:55",
+        src: "http://spoti4.future4.com.br/24.mp3" 
+    },
+    {
+        title: "Música 25",
+        artist: "Artista 25",
+        duration: "1:42",
+        src: "http://spoti4.future4.com.br/25.mp3" 
+    },
+    {
+        title: "Música 26",
+        artist: "Artista 26",
+        duration: "2:32",
+        src: "http://spoti4.future4.com.br/26.mp3" 
+    },
+    {
+        title: "Música 27",
+        artist: "Artista 27",
+        duration: "2:04",
+        src: "http://spoti4.future4.com.br/27.mp3" 
+    },
+    {
+        title: "Música 28",
+        artist: "Artista 28",
+        duration: "3:55",
+        src: "http://spoti4.future4.com.br/28.mp3" 
+    },
+    {
+        title: "Música 29",
+        artist: "Artista 29",
+        duration: "2:17",
+        src: "http://spoti4.future4.com.br/29.mp3" 
+    },
+    {
+        title: "Música 30",
+        artist: "Artista 30",
+        duration: "2:19",
+        src: "http://spoti4.future4.com.br/30.mp3" 
+    },
+    {
+        title: "Música 31",
+        artist: "Artista 31",
+        duration: "2:24",
+        src: "http://spoti4.future4.com.br/31.mp3" 
+    },
+    {
+        title: "Música 32",
+        artist: "Artista 32",
+        duration: "2:21",
+        src: "http://spoti4.future4.com.br/32.mp3" 
+    },
+    {
+        title: "Música 33",
+        artist: "Artista 33",
+        duration: "2:47",
+        src: "http://spoti4.future4.com.br/33.mp3" 
+    },
+    {
+        title: "Música 34",
+        artist: "Artista 34",
+        duration: "2:50",
+        src: "http://spoti4.future4.com.br/34.mp3" 
+    },
+    {
+        title: "Música 35",
+        artist: "Artista 35",
+        duration: "2:30",
+        src: "http://spoti4.future4.com.br/35.mp3" 
+    },
+    {
+        title: "Música 36 - rap?",
+        artist: "Artista 36",
+        duration: "2:36",
+        src: "http://spoti4.future4.com.br/36.mp3" 
+    },
+    {
+        title: "Música 37",
+        artist: "Artista 37",
+        duration: "2:48",
+        src: "http://spoti4.future4.com.br/37.mp3" 
+    },
+    {
+        title: "Música 38",
+        artist: "Artista 38",
+        duration: "1:51",
+        src: "http://spoti4.future4.com.br/38.mp3" 
+    },
+    {
+        title: "Música 39",
+        artist: "Artista 39",
+        duration: "2:41",
+        src: "http://spoti4.future4.com.br/39.mp3" 
+    },
+    {
+        title: "Música 40",
+        artist: "Artista 40",
+        duration: "2:05",
+        src: "http://spoti4.future4.com.br/40.mp3" 
+    },
+    {
+        title: "Música 45",
+        artist: "Artista 45",
+        duration: "2:00",
+        src: "http://spoti4.future4.com.br/45.mp3" 
+    },
+    {
+        title: "Música 50",
+        artist: "Artista 50",
+        duration: "3:38",
+        src: "http://spoti4.future4.com.br/50.mp3" 
+    },
+    {
+        title: "Música 55",
+        artist: "Artista 55",
+        duration: "2:27",
+        src: "http://spoti4.future4.com.br/55.mp3" 
+    },
+    {
+        title: "Música 60",
+        artist: "Artista 60",
+        duration: "3:01",
+        src: "http://spoti4.future4.com.br/60.mp3" 
+    },
+    {
+        title: "Música 65",
+        artist: "Artista 65",
+        duration: "3:05",
+        src: "http://spoti4.future4.com.br/65.mp3" 
+    },
+    {
+        title: "Música 70",
+        artist: "Artista 70",
+        duration: "3:03",
+        src: "http://spoti4.future4.com.br/70.mp3" 
+    },
+    {
+        title: "Música 75",
+        artist: "Artista 75",
+        duration: "1:56",
+        src: "http://spoti4.future4.com.br/75.mp3" 
+    },
+    {
+        title: "Música 80",
+        artist: "Artista 80",
+        duration: "3:03",
+        src: "http://spoti4.future4.com.br/80.mp3" 
+    },
+    // {
+    //     title: "Música 81",
+    //     artist: "Artista 81",
+    //     duration: "15:05",
+    //     src: "http://spoti4.future4.com.br/81.mp3" 
+    // },
+    {
+        title: "Música 82",
+        artist: "Artista 82",
+        duration: "2:38",
+        src: "http://spoti4.future4.com.br/82.mp3" 
+    },
+    {
+        title: "Música 83",
+        artist: "Artista 83",
+        duration: "1:30",
+        src: "http://spoti4.future4.com.br/83.mp3" 
+    },
+    {
+        title: "Música 84",
+        artist: "Artista 84",
+        duration: "3:02",
+        src: "http://spoti4.future4.com.br/84.mp3" 
+    },
+    {
+        title: "Música 85",
+        artist: "Artista 85",
+        duration: "2:21",
+        src: "http://spoti4.future4.com.br/85.mp3" 
+    },
+    {
+        title: "Música 86",
+        artist: "Artista 86",
+        duration: "2:15",
+        src: "http://spoti4.future4.com.br/86.mp3" 
+    },
+    {
+        title: "Música 87",
+        artist: "Artista 87",
+        duration: "1:47",
+        src: "http://spoti4.future4.com.br/87.mp3" 
+    },
+    {
+        title: "Música 88",
+        artist: "Artista 88",
+        duration: "1:32",
+        src: "http://spoti4.future4.com.br/88.mp3" 
+    },
+    {
+        title: "Música 89",
+        artist: "Artista 89",
+        duration: "3:13",
+        src: "http://spoti4.future4.com.br/89.mp3" 
+    },
+    {
+        title: "Música 90",
+        artist: "Artista 90",
+        duration: "2:30",
+        src: "http://spoti4.future4.com.br/90.mp3" 
+    },
+    {
+        title: "Música 91",
+        artist: "Artista 91",
+        duration: "2:30",
+        src: "http://spoti4.future4.com.br/91.mp3" 
+    },
+    {
+        title: "Música 92",
+        artist: "Artista 92",
+        duration: "1:36",
+        src: "http://spoti4.future4.com.br/92.mp3" 
+    },
+    {
+        title: "Música 93",
+        artist: "Artista 93",
+        duration: "1:50",
+        src: "http://spoti4.future4.com.br/93.mp3" 
+    },
+    {
+        title: "Música 94",
+        artist: "Artista 94",
+        duration: "1:50",
+        src: "http://spoti4.future4.com.br/94.mp3" 
+    },
+    {
+        title: "Música 95",
+        artist: "Artista 95",
+        duration: "2:06",
+        src: "http://spoti4.future4.com.br/95.mp3" 
+    },
+    {
+        title: "Música 96",
+        artist: "Artista 96",
+        duration: "3:03",
+        src: "http://spoti4.future4.com.br/96.mp3" 
+    },
+    {
+        title: "Música 97",
+        artist: "Artista 97",
+        duration: "3:03",
+        src: "http://spoti4.future4.com.br/97.mp3" 
+    },
+    {
+        title: "Música 98",
+        artist: "Artista 98",
+        duration: "0:08",
+        src: "http://spoti4.future4.com.br/98.mp3" 
     }
 ]
